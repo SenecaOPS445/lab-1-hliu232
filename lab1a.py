f818dd8e09c782dc7adf2c34a7c865af57b1ebb1
Haiwei Liu <hliu232@myseneca.ca>
2025-01-13 21:04:08 -0500

    Added lab1a

--- lab1a.py
+++ lab1a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-'''Description: This program will output "hello world" to the screen.'''
+'''
+Description: This program will output "hello world" to the screen.
+'''
 
 # Any line that starts with a "#" is also known as a comment,
 # these lines are ignored by the python interpreter even if
